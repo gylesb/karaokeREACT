04e99d4c97a1fceeb8101dae00a002343ffd1991
Gyles Batara <gyles.batara@live.com>
2018-02-20 09:54:03 -0800

    Set initial State

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+// CHORUS INFO
+const songLyricsArray = "So don't call me baby, Unless you mean it, Don't tell me you need me,  If you don't believe it, So let me know the truth, Before I dive right into you".split(', ')
+
+// INITIAL REDUX STATE
+const initalState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
+}
+
+console.log(initalState);
+
+// CLICK LISTENER
 const userClick = () => {
   console.log('click');
 }
